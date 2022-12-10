dfa02b36ff7a0c06eff1344bee9b15a31545d406
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-09 17:23:47 -0700

    Started portfolio page, needs additional work

---
+++ src/components/pages/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Portfolio extends Component {
+  render() {
+    return <h1>Need to build portfolio page</h1>;
+  }
+}
+
+export default Portfolio;
