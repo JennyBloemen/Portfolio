d2d4a477df39223f5e93b744f650e5d2db273a82
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-12 21:01:23 -0700

    Cleaned unecessary code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from "react";
-// import logo from "./logo.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
 import PortfolioContainer from "./components/PortfolioContainer";
 
 const App = () => <PortfolioContainer />;
 
 export default App;
-
-// import Home from "./components/pages/Home";
-// import contact from "./components/Contact";
-// import Navbar from "./components/Navbar";
-// import Navbar from "./components/Navbar";
-// function App() {
-//   return (
-//     <div>
-//       <Home />
-//       {/* <Contact /> */}
-//       <Navbar />
-//     </div>
-//   );
-// }
