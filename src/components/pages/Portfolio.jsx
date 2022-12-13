4fb8df707a8b5503e51ef394715a01e9f824ae84
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-12 17:17:32 -0700

    updated file name

--- src/components/pages/Portfolio.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Portfolio extends Component {
-  render() {
-    return <h1>Need to build portfolio page</h1>;
-  }
-}
-
-export default Portfolio;
