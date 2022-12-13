efe5592600ac7a95eb166d94a2560c5351119810
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-12 22:31:42 -0700

    Removed extra code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,6 @@ export default function Projects({ data }) {
   const { title, description, img, github, deployed } = data;
   return (
     <div>
-      {/* <h3>{title}</h3>
-      <p>{description}</p>
-      <img src={img} alt="project image"></img>
-      <a>{github}</a>
-      <a>{deployed}</a> */}
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={img} />
         <Card.Body>
@@ -26,29 +21,3 @@ export default function Projects({ data }) {
     </div>
   );
 }
-
-// function KitchenSinkExample() {
-//   return (
-//     <Card style={{ width: '18rem' }}>
-//       <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-//       <Card.Body>
-//         <Card.Title>Card Title</Card.Title>
-//         <Card.Text>
-//           Some quick example text to build on the card title and make up the
-//           bulk of the card's content.
-//         </Card.Text>
-//       </Card.Body>
-//       <ListGroup className="list-group-flush">
-//         <ListGroup.Item>Cras justo odio</ListGroup.Item>
-//         <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-//         <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-//       </ListGroup>
-//       <Card.Body>
-//         <Card.Link href="#">Card Link</Card.Link>
-//         <Card.Link href="#">Another Link</Card.Link>
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-
-// export default KitchenSinkExample;
