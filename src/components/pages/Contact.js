6146ee51dbe1f97de05915768564c45065ba7e2b
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-12 23:40:10 -0700

    Got email link working through Formspree

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Contact extends Component {
   constructor(props) {
     super(props);
     this.submitForm = this.submitForm.bind(this);
     this.state = {
-      status: ""
+      status: "",
     };
   }
-
 
   submitForm(ev) {
     ev.preventDefault();
@@ -34,35 +33,65 @@ class Contact extends Component {
     return (
       <div className="bg-dark text-white" id="contactSect">
         <div className="container" id="contact">
-          <h2 className="contact text-center">Contact Me</h2>
+          <h3 className="contact text-center">Please Drop a Message</h3>
 
           <form
             className="contact-form"
             onSubmit={this.submitForm}
-            action="https://formspree.io/xknqzwvk"
-            method="POST">
+            action="https://formspree.io/f/xpznvekb"
+            method="POST"
+          >
             <div className="form-group">
               <label>Name</label>
-              <input type="text" className="form-control" id="exampleInputName1" name="name" placeholder="Your Name" />
-
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputName1"
+                name="name"
+                placeholder="Your Name"
+              />
             </div>
 
             <div className="form-group">
               <label>Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" name="email" placeholder="email@gmail.com" />
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                name="email"
+                placeholder="email@gmail.com"
+              />
             </div>
 
             <div className="form-group">
               <label>Message</label>
-              <textarea name="message" className="form-control" id="exampleFormControlTextarea1" rows="6" />
+              <textarea
+                name="message"
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="12"
+              />
             </div>
 
-            {status === "SUCCESS" ? <p className="success">Thank you for sending me a message!</p> : <button className="contactBtn">Submit</button>}
-            {status === "ERROR" && <p className="failure">Ooops! There was an error. Please fill out all the fields and try againy</p>}
+            {status === "SUCCESS" ? (
+              <p className="success">Thank you for sending me a message!</p>
+            ) : (
+              <button className="contactBtn">Submit</button>
+            )}
+            {status === "ERROR" && (
+              <p className="failure">
+                Ooops! There was an error. Please fill out all the fields and
+                try again.
+              </p>
+            )}
           </form>
           <div className="contactInfo text-center">
-            <span>Email: </span><a href="mailto:jennyjbloemen@gmail.com">jennybloemen@gmail.com</a><br />
-            <span>Call Me: </span><a href="tel:(801)2016307">(801)201-6307</a><br />
+            <span>Email: </span>
+            <a href="mailto:jennyjbloemen@gmail.com">jennyjbloemen@gmail.com</a>
+            <br />
+            <span>Call Me: </span>
+            <a href="tel:(801)2016307">(801)201-6307</a>
+            <br />
             <span>Download my resume:</span>
             {/* <a href={Resume} target='_blank' rel="noopener noreferrer">
               <button className="btn"
@@ -70,9 +99,24 @@ class Contact extends Component {
                 <i className="fa fa-download resume"></i></button>
             </a> */}
             <div className="social-links">
-              <span><a href="https://github.com/jennybloemen" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-3x"></i></a></span>
-              <span><a href="https://www.linkedin.com/in/jenny-bloemen/" target="_blank" rel="noopener noreferrer"><i
-                className="fa fa-linkedin fa-2x"></i></a></span>
+              <span>
+                <a
+                  href="https://github.com/jennybloemen"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-github fa-3x"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://www.linkedin.com/in/jenny-bloemen/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-linkedin fa-2x"></i>
+                </a>
+              </span>
 
               {/* <span>Download my resume:</span><a class="resume" href="AlexResume.pdf" target="_blank"><button><i
                   class="fa fa-download"></i></button></a> */}
@@ -80,8 +124,7 @@ class Contact extends Component {
           </div>
         </div>
       </div>
-
     );
   }
 }
-export default Contact
+export default Contact;
