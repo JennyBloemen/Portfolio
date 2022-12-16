80c659d63a30d8da09846b6684b48c3b206e8c12
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-15 20:45:02 -0700

    Implemented multi image carosel

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import Projects from "../Projects";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 export default function Portfolio() {
   const projects = [
@@ -15,57 +35,75 @@ export default function Portfolio() {
       deployed: "link",
     },
     {
-      title: "Project 2",
+      title: "Project 3",
       description: "description",
       img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
       github: "link",
       deployed: "link",
     },
     {
-      title: "Project 2",
+      title: "Project 4",
       description: "description",
       img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
       github: "link",
       deployed: "link",
     },
     {
-      title: "Project 2",
+      title: "Project 5",
       description: "description",
       img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
       github: "link",
       deployed: "link",
     },
     {
-      title: "Project 2",
+      title: "Project 6",
       description: "description",
       img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
       github: "link",
       deployed: "link",
     },
     {
-      title: "Project 2",
+      title: "Project 7",
       description: "description",
       img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
       github: "link",
       deployed: "link",
     },
   ];
+
   return (
     <div>
       <div class="portfolio-header">
         <h3>Coding Projects</h3>
         <p></p>
       </div>
-      <div class="portfolio-container">
+      <Carousel
+        className="carosel_container"
+        centerMode={true}
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        // deviceType={this.props.deviceType}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-20-px"
+      >
         {projects.map((data, idx) => (
           <Projects data={data} key={idx} />
         ))}
-      </div>
-      <div class="portfolio-header">
+      </Carousel>
+      <div className="portfolio-header">
         <h3>Design Work</h3>
         <p></p>
       </div>
     </div>
   );
 }
-// md={{ span: 6, offset: 3 }}
