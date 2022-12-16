629a60a7c375564003b5c6e654bad351c388f633
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-15 20:43:00 -0700

    css changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,18 +4,18 @@ import Card from "react-bootstrap/Card";
 export default function Projects({ data }) {
   const { title, description, img, github, deployed } = data;
   return (
-    <div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={img} />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{description}</Card.Text>
-        </Card.Body>
-        <Card.Body>
-          <Card.Link href="#">Github:{github}</Card.Link>
-          <Card.Link href="#">Deployed:{deployed}</Card.Link>
-        </Card.Body>
-      </Card>
-    </div>
+    <Card className="m-3" style={{ maxWidth: "18rem" }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Link href="#">Github:{github}</Card.Link>
+        <Card.Link href="#">Deployed:{deployed}</Card.Link>
+      </Card.Body>
+    </Card>
   );
 }
+
+//
