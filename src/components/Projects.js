8d61d1c6cce3ac939e2806f1f3ed6155c59258b7
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-16 19:24:00 -0700

    Added projects to template

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 export default function Projects({ data }) {
-  const { title, description, img, github, deployed } = data;
+  const { title, description, img, github } = data;
   return (
-    <Card className="m-3" style={{ maxWidth: "18rem" }}>
-      <Card.Img variant="top" src={img} />
+    <Card className="m-3" style={{ Width: "25rem", Height: "25rem" }}>
+      <Card.Img
+        variant="top"
+        src={img}
+        style={{ Width: "25rem", Height: "25rem" }}
+      />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
-      </Card.Body>
-      <Card.Body>
-        <Card.Link href="#">Github:{github}</Card.Link>
-        <Card.Link href="#">Deployed:{deployed}</Card.Link>
+        {/* //need to figure out button link  */}
+        <div className="d-grid gap-2">
+          {/* <Link to="../../../Portfolio.js"></Link> */}
+          <a href={github} target="_blank" rel="noreferrer"></a>
+          <Button variant="secondary" size="sm">
+            GitHub
+          </Button>
+        </div>
       </Card.Body>
     </Card>
   );
