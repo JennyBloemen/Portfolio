f10c99cf11439dafa62ec82174f8eb831f69d369
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-16 19:24:48 -0700

    removed additional carosel

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,8 +1,7 @@
 import React from "react";
 import Projects from "../Projects";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
+import Marketing from "../Projects";
+
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
@@ -26,48 +25,73 @@ const responsive = {
 
 export default function Portfolio() {
   const projects = [
-    // const [projects, setProjects] = useState([
     {
-      title: "Project 1",
-      description: "description",
-      img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
+      title: "GoGroom",
+      description: "Animal grooming servies schedular",
+      img: require(`../../assets/images/dog.png`),
       github: "link",
-      deployed: "link",
     },
     {
-      title: "Project 3",
-      description: "description",
+      title: "FitCommit",
+      description: "Restful API fitness schedular.",
       img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
-      github: "link",
-      deployed: "link",
+      github: "https://github.com/JennyBloemen/FitCommit.git",
     },
     {
-      title: "Project 4",
+      title: "MuSearch",
       description: "description",
-      img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
-      github: "link",
-      deployed: "link",
+      img: require(`../../assets/images/MuSearchDemo.gif`),
+      github: "https://github.com/JennyBloemen/MuSearch.git",
     },
     {
-      title: "Project 5",
-      description: "description",
-      img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
-      github: "link",
-      deployed: "link",
+      title: "Weather Dashboard",
+      description: "Open API weather dashboard.",
+      img: require(`../../assets/images/WeatherDashboard.png`),
+      github: "https://github.com/JennyBloemen/JB-Weather.git",
     },
     {
-      title: "Project 6",
-      description: "description",
-      img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
-      github: "link",
-      deployed: "link",
+      title: "Daily Work Schedular",
+      description: "App to schedule each hour in the day",
+      img: require(`../../assets/images/DailySchedular.png`),
+      github: "https://github.com/JennyBloemen/JBSchedular.git",
     },
     {
-      title: "Project 7",
+      title: "Employee Tracker",
+      description:
+        "Command-line application using Node.js, Inquirer, and MySQL",
+      img: require(`../../assets/images/EmployeeTrackingWalkthrough.gif`),
+      github: "https://github.com/JennyBloemen/EmployeeTracking.git",
+      // deployed: "link",
+    },
+  ];
+  const marketing = [
+    {
+      title: "Baba Yaga",
       description: "description",
       img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
       github: "link",
-      deployed: "link",
+      // deployed: "link",
+    },
+    {
+      title: "Baba Yaga",
+      description: "description",
+      img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
+      github: "link",
+      // deployed: "link",
+    },
+    {
+      title: "Baba Yaga",
+      description: "description",
+      img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
+      github: "link",
+      // deployed: "link",
+    },
+    {
+      title: "Baba Yaga",
+      description: "description",
+      img: require(`../../assets/images/FitCommit-Homepage-Screenshot.png`),
+      github: "link",
+      // deployed: "link",
     },
   ];
 
@@ -75,6 +99,34 @@ export default function Portfolio() {
     <div>
       <div class="portfolio-header">
         <h3>Coding Projects</h3>
+        <p></p>
+      </div>
+      <Carousel
+        className="carosel_container"
+        centerMode={true}
+        swipeable={true}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        // deviceType={this.props.deviceType}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-20-px"
+      >
+        {projects.map((data, idx) => (
+          <Projects data={data} key={idx} />
+        ))}
+      </Carousel>
+      {/* <br></br>
+      <div className="portfolio-header">
+        <h3>Marketing Design Work</h3>
         <p></p>
       </div>
       <Carousel
@@ -96,14 +148,10 @@ export default function Portfolio() {
         dotListClass="custom-dot-list-style"
         itemClass="carousel-item-padding-20-px"
       >
-        {projects.map((data, idx) => (
+        {marketing.map((data, idx) => (
           <Projects data={data} key={idx} />
         ))}
-      </Carousel>
-      <div className="portfolio-header">
-        <h3>Design Work</h3>
-        <p></p>
-      </div>
+      </Carousel> */}
     </div>
   );
 }
